dc69027bbbab5e3f5bd32dab4268c80513dc8523
khaledh150 <khaledhammad150@gmail.com>
2026-02-21 12:01:39 +0700

    Refactor deployment to use gh-pages package

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/flsahcardbasic/',
-  build: {
-    outDir: 'docs',
-  },
   plugins: [react(), tailwindcss()],
 })
